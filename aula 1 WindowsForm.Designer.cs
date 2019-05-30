0ded818488999f7c1e6b97051e6637952ef03ae4
Rovani01 <rovanicajc@gmail.com>
2019-05-29 21:32:35 -0300

    AULA 1

---
+++ aula 1 WindowsForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Aula.Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(317, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AULA FORMS";
+            // 
+            // btnOk
+            // 
+            this.btnOk.AutoEllipsis = true;
+            this.btnOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnOk.Location = new System.Drawing.Point(288, 124);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(191, 43);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "OK!";
+            this.btnOk.UseVisualStyleBackColor = false;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(800, 618);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Primeiro Progama Forms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
